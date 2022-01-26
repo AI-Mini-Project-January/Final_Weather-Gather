b8c6385e04d39518349614988f94cbb949e29a90
WooriHCho <woori.helena.cho@gmail.com>
2022-01-27 03:02:48 +0900

    로그인 부분에서 카카오 기능 뺐습니다

--- Weather_n_Gather/user/urls.py
+++ Weather_n_Gather/user/urls.py
@@ -8,17 +8,4 @@ urlpatterns = [
     path('login', Login.as_view()),
     path('logout', Logout.as_view()),
     path('profile/upload', UploadProfile.as_view()),
-
-
-    #카카오 메시지 보내기 위한 로그인
-    path('user/kakaologinhome/', views.kakaologinHome),
-    path('kakaoLoginLogic/', views.kakaoLoginLogic),
-    path('kakaoLoginLogicRedirect/', views.kakaoLoginLogicRedirect),
-    path('kakaoLogout/', views.kakaoLogout),
-    #날씨 요약 정보
-    path('kakaomessage_climate/', views.kakaoMessage_climate),
-    #패스워드 찾기
-    path('loginSuccess/', views.password_throw),
-    # path('passwordckeck/', views.password_check),
-    path('password/', views.kakaoMessage_password),
 ]
