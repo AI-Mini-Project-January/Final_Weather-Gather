da4fe25186fd339c96d5beea7e9ed636648fcde8
WooriHCho <woori.helena.cho@gmail.com>
2022-01-27 16:30:10 +0900

    user 충돌 수정

--- WnG/user/urls.py
+++ WnG/user/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 from .views import Join, Login, Logout, UploadProfile 
 from . import views
-<<<<<<< HEAD
-
-=======
->>>>>>> e38e1c0fef9a6a69c3f500835f681bc3f09ecf03
 # urls를 join이라고 해도 앱이름이 user이기 때문에 ~/user/join 으로 들어감
 urlpatterns = [
     path('join', Join.as_view()),
